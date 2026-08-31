2b9918e5318dba131ee7adb33ba92ac78556948e
Kim geonhui <kimgunhee6@gmail.com>
2026-08-31 20:29:36 +0900

    week2_advanced_03,04 solved
    krafton_junle_week2_advanced_03,04 solved

--- week2/2. advanced/03_priority_queue.py
+++ week2/2. advanced/03_priority_queue.py
@@ -38,19 +38,26 @@ def process_emergency_room(patients):
     """
     # TODO: 빈 힙 생성
     heap = []
-    
-    
-    # TODO: 모든 환자를 힙에 추가
-    pass
         
+    # TODO: 모든 환자를 힙에 추가
+    for x in patients:
+        name = x[0]
+        priority = x[1]
+        heapq.heappush(heap, (priority, name))    
+
     processed = []
     
     # TODO: 힙이 비어있지 않은 동안 반복
     ## 힙에서 우선순위가 가장 높은 환자 꺼내기
     ## 환자 처리
-    pass
-        
+    # heap에서 작은 원소를 순서대로 빼면서 result에 저장 비어있는 경우 Error
+    while heap:
+        priority, name = heapq.heappop(heap)
+        print(f"처리: {name} (우선순위: {priority})")
+        processed.append((name))
+    
     return processed
+
 
 # 테스트 케이스
 if __name__ == "__main__":
--- week2/2. advanced/04_hanoi_tower.py
+++ week2/2. advanced/04_hanoi_tower.py
@@ -60,7 +60,7 @@
 def hanoi_count(n: int) -> int:
     """N 개의 원반을 옮기는 데 필요한 최소 이동 횟수( = 2^N - 1) 를 반환"""
     # TODO: 2^N - 1 을 정수로 반환하세요.
-    pass
+    return 2**n -1
 
 
 def hanoi_moves(n: int) -> list:
@@ -71,10 +71,25 @@ def hanoi_moves(n: int) -> list:
 
     예) hanoi_moves(2) == [(1, 2), (1, 3), (2, 3)]
     """
-    # TODO: N > 20 또는 N == 0 인 경우 [] 를 반환하세요.
-    # TODO: 그 외에는 재귀로 이동 순서를 만들어 반환하세요.
-    pass
+    result = []
 
+    # TODO: N > 20 또는 N == 0 인 경우 [] 를 반환하세요.
+    if n>20 or n==0:
+        return []
+    # TODO: 그 외에는 재귀로 이동 순서를 만들어 반환하세요.
+    # 함수 1
+    def move(n, start, end, extra):
+        if n == 1:
+            return result.append((start,end))
+        else:
+            move(n-1, start, extra, end)
+            result.append([start, end])
+            move(n-1, extra, end, start)
+
+    # 함수 1 호출
+    move(n, 1, 3, 2)
+    
+    return result
 
 if __name__ == "__main__":
     print("[테스트 1] N=0 (원반 없음, 옮길 것 없음)")
