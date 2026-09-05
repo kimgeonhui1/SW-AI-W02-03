2a14b27659d904aeed26546ad0c7ab4344ee2c81
Kim geonhui <kimgunhee6@gmail.com>
2026-09-05 21:11:39 +0900

    krafton_junle_week3_basic_01~04 solved
    krafton_junle_week3_basic_01~04 solved

--- week3/1. basic/03_graph_basic.py
+++ week3/1. basic/03_graph_basic.py
@@ -40,14 +40,24 @@ def create_graph(vertices, edges, directed=False):
     Returns:
         그래프 딕셔너리
     """
+
+    graph = {}
+    
     # TODO: 빈 그래프 초기화
-    pass
+    graph.clear()
     
     # TODO: 간선 추가
     ## 간선 추가 (u에서 v로)
+    for i in range(vertices):
+        graph[i] = []
+
+    for start, end in edges:
+        graph[start].append(end)
+
     ## 무방향 그래프면 반대 방향도 추가
-    pass
-    
+        if directed == False:
+            graph[end].append(start)
+
     return graph
 
 # 테스트 케이스
--- week3/1. basic/04_bfs.py
+++ week3/1. basic/04_bfs.py
@@ -44,16 +44,23 @@ def bfs(graph, start):
         방문 순서 리스트
     """
     visited = []
-    
+    k = 0 
+
     # TODO: 큐 생성 및 시작 정점 추가
     ## 방문한 정점 집합
-    pass
+    queue = deque([start])
+    visited = [start]
 
     # TODO: 큐가 빌 때까지 반복
     ## 큐에서 정점 꺼내기
     ## 인접한 정점들 확인
     ## 방문하지 않은 정점이면 큐에 추가
-    pass
+    while queue:
+        current = queue.popleft()
+        for neighbor in graph[current]:
+            if neighbor not in visited:
+                visited.append(neighbor)
+                queue.append(neighbor)
     
     return visited
 
