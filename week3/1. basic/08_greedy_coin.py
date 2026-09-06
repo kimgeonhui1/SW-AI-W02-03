948c5a381aae6983bdec87ab1f2ee5cc300ca57c
Kim geonhui <kimgunhee6@gmail.com>
2026-09-06 19:24:22 +0900

    krafton_jungle_weeek3_basic_08 solved
    krafton_jungle_weeek3_basic_08 solved

--- week3/1. basic/08_greedy_coin.py
+++ week3/1. basic/08_greedy_coin.py
@@ -43,11 +43,21 @@ def make_change_greedy(change, coins):
     result = {}
     total_coins = 0
     
+    coin_sort = sorted(coins, reverse = True)
+
     # TODO: 각 동전에 대해 반복
     ## 현재 동전으로 거슬러줄 수 있는 개수 계산    
     ## 개수가 0보다 크면 결과에 추가
-    pass
-    
+    for coin in coin_sort:
+        # 몫
+        a = change // coin
+        
+        if a > 0:
+            result[coin] = a
+            total_coins += a
+
+            change %= coin
+
     return total_coins, result
 
 # 테스트 케이스
